0980b534796cc55bf2e437183d24239706588bc4
TheClashFruit <clash.fruit123@gmail.com>
2023-12-03 10:20:03 +0100

    feat: add logging or something

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{App, HttpServer, web};
+use actix_cors::Cors;
+use actix_web::{App, http, HttpServer, web};
+use actix_web::middleware::Logger;
 use dotenv::dotenv;
 use crate::routes::index::index;
 use crate::routes::not_found::not_found;
@@ -12,10 +14,27 @@ mod structs;
 async fn main() -> std::io::Result<()> {
   dotenv().ok();
 
+  env_logger::init_from_env(
+    env_logger::Env::new()
+      .default_filter_or("info")
+  );
+
   let bind_address = std::env::var("BIND").expect("PORT must be set.");
 
   HttpServer::new(|| {
+    let cors = Cors::default()
+      .allow_any_origin()
+      .allow_any_method()
+      .allowed_headers(vec![
+        http::header::AUTHORIZATION,
+        http::header::ACCEPT,
+        http::header::CONTENT_TYPE
+      ])
+      .max_age(3600);
+
     App::new()
+      .wrap(cors)
+      .wrap(Logger::default())
       .service(index)
       .service(changelog)
       .service(swagger)
--- src/routes/not_found.rs
+++ src/routes/not_found.rs
@@ -1,9 +1,12 @@
+use actix_web::http::header::ContentType;
+use actix_web::HttpResponse;
 use actix_web::web::Json;
 use crate::structs::not_found::NotFoundRes;
 
-pub async fn not_found() -> Json<NotFoundRes> {
-  Json(NotFoundRes {
-    status: 404,
-    message: String::from("Route not found.")
-  })
+pub async fn not_found() -> HttpResponse {
+  HttpResponse::NotFound()
+    .json(NotFoundRes {
+      status: 404,
+      message: String::from("Route not found.")
+    })
 }
