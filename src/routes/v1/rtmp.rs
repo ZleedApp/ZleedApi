5b80a56d4ce121688018b8d13c8dbed9cf42f4ee
TheClashFruit <clash.fruit123@gmail.com>
2024-02-17 15:16:06 +0100

    feat: add `RTMP_IP` to an env var

--- src/routes/v1/rtmp.rs
+++ src/routes/v1/rtmp.rs
@@ -3,7 +3,14 @@ use actix_web::http::header::ContentType;
 
 #[post("/v1/rtmp/publish")]
 pub async fn rtmp_publish(req: HttpRequest) -> impl Responder  {
+  let ip = std::env::var("RTMP_IP").expect("RTMP_IP must be set.");
+
+  // TODO: get username from stream key.
+  let username = "username";
+
+  let rtmp_url = format!("rtmp://{}/live/{}", ip, username);
+
   HttpResponse::Found()
-    .append_header(("Location", "rtmp://rtmp.zleed.tv/live/username"))
+    .append_header(("Location", rtmp_url))
     .finish()
 }
--- src/structs/db/user.rs
+++ src/structs/db/user.rs
@@ -10,6 +10,7 @@ pub struct User {
   pub(crate) password: String,
   pub(crate) avatar: String,
   pub(crate) badges: Vec<Badge>,
+  pub(crate) stream_keys: Vec<StreamKeys>,
   pub(crate) last_live: String,
   pub(crate) created_at: String,
   pub(crate) updated_at: String
@@ -21,5 +22,13 @@ pub struct Badge {
   pub(crate) id: u32,
   pub(crate) image: String,
   pub(crate) name: String,
+  pub(crate) description: String
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct StreamKeys {
+  pub(crate) id: u32,
+  pub(crate) key: String,
   pub(crate) description: String
 }
