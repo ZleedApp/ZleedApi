3cefe052c2e2e09e2e438293aa2f825ffe7ad815
TheClashFruit <clash.fruit123@gmail.com>
2024-02-17 14:53:23 +0100

    fix: change redirect res code to `302 Found`

--- src/routes/v1/rtmp.rs
+++ src/routes/v1/rtmp.rs
@@ -3,7 +3,7 @@ use actix_web::http::header::ContentType;
 
 #[post("/v1/rtmp/publish")]
 pub async fn rtmp_publish(req: HttpRequest) -> impl Responder  {
-  HttpResponse::TemporaryRedirect()
+  HttpResponse::Found()
     .append_header(("Location", "rtmp://rtmp.zleed.tv/live/username"))
     .finish()
 }
